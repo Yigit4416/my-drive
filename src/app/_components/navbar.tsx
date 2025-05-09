5c2332df6fbfa118e9e2368494c7abdd9e164600
Yigit4416 <yigit.ozdemir44@icloud.com>
2025-05-09 10:40:44 +0300

    fixed breadcrumb

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -18,6 +18,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
+import Link from "next/link";
 
 export function NavBar() {
   const pathname = usePathname();
@@ -34,24 +35,7 @@ export function NavBar() {
     <div className="mt-0.5">
       <Breadcrumb>
         <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/">Home</BreadcrumbLink>
-          </BreadcrumbItem>
-          {pathSegments.map((segment, index) => {
-            const href = "/" + pathSegments.slice(0, index + 1).join("/");
-            return (
-              <Fragment key={href}>
-                <BreadcrumbSeparator>
-                  <Slash />
-                </BreadcrumbSeparator>
-                <BreadcrumbItem>
-                  <BreadcrumbLink href={"/folder" + href}>
-                    {segment}
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-              </Fragment>
-            );
-          })}
+          <LongBreadcrumb pathSegments={pathSegments} />
         </BreadcrumbList>
       </Breadcrumb>
     </div>
@@ -63,40 +47,51 @@ function LongBreadcrumb({ pathSegments }: { pathSegments: string[] }) {
   if (lenght > 2) {
     const visibleContent =
       pathSegments.length > 2 ? pathSegments.slice(-2) : pathSegments;
-    const dropdownContent = pathSegments.slice(lenght - 2);
+    const dropdownContent = pathSegments.slice(0, lenght - 2);
+    const dropdownLength = dropdownContent.length;
     return (
-      <div>
+      <>
         <DropdownMenu>
-          <DropdownMenuTrigger className="flex items-center gap-1">
-            <BreadcrumbEllipsis className="h-4 w-4" />
+          <DropdownMenuTrigger className="flex items-center space-x-1">
+            <BreadcrumbEllipsis className="h-4 w-4 text-gray-500" />
             <span className="sr-only">Toggle Menu</span>
           </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            {dropdownContent.map((drop) => {
-              const href = "/";
-              return <div></div>;
+          <DropdownMenuContent className="rounded-md bg-white p-2 shadow-md">
+            {dropdownContent.map((drop, index) => {
+              const href = "/" + pathSegments.slice(0, index + 2).join("/");
+              return (
+                <Link href={"/folder" + href} prefetch={true}>
+                  <DropdownMenuItem
+                    key={href}
+                    className="px-4 py-2 hover:bg-gray-100"
+                  >
+                    {drop}
+                  </DropdownMenuItem>
+                </Link>
+              );
             })}
           </DropdownMenuContent>
         </DropdownMenu>
         {visibleContent.map((item, index) => {
-          // Need to find out about these paths
-          const href = "/" + pathSegments.slice(0, index + 1).join("/");
+          const href =
+            "/folder/" +
+            pathSegments.slice(0, index + dropdownLength).join("/");
           return (
             <Fragment key={href}>
-              <BreadcrumbSeparator>
+              <BreadcrumbSeparator className="text-gray-400">
                 <Slash />
               </BreadcrumbSeparator>
               <BreadcrumbItem>
-                <BreadcrumbLink href={"/folder" + href}>{item}</BreadcrumbLink>
+                <BreadcrumbLink href={href}>{item}</BreadcrumbLink>
               </BreadcrumbItem>
             </Fragment>
           );
         })}
-      </div>
+      </>
     );
   } else {
     return (
-      <div>
+      <>
         {pathSegments.map((segment, index) => {
           const href = "/" + pathSegments.slice(0, index + 1).join("/");
           return (
@@ -112,7 +107,7 @@ function LongBreadcrumb({ pathSegments }: { pathSegments: string[] }) {
             </Fragment>
           );
         })}
-      </div>
+      </>
     );
   }
 }
